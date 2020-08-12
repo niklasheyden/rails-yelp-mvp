f0ec48cc5afd033c6e1647fc13b6fc84ae9b6536
Niklas von Heyden <niklas.heyden@me.com>
2020-08-12 12:44:29 +0200

    rails new

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,2 @@
 class Restaurant < ApplicationRecord
-  CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
-  has_many :reviews, dependent: :destroy
-  validates :name, :address, :category, presence: true
-  validates :category, inclusion: { in: CATEGORIES }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,3 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
-  validates :restaurant, :content, presence: true
-  validates :rating, inclusion: { in: (0..5), allow_nil: false }, numericality: { only_integer: true }
 end
