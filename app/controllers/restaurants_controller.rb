1006393db8a4029ec1efca55907d17720decdcc7
Niklas von Heyden <niklas.heyden@me.com>
2020-08-12 16:50:39 +0200

    refactoring

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,12 +1,12 @@
 
 class RestaurantsController < ApplicationController
-  before_action :set_restaurant, only: [:show]
 
   def index
     @restaurants = Restaurant.all
   end
 
   def show
+    @restaurant = Restaurant.find(params[:id])
     @reviews = @restaurant.reviews
   end
 
@@ -24,10 +24,6 @@ class RestaurantsController < ApplicationController
   end
 
   private
-
-  def set_restaurant
-    @restaurant = Restaurant.find(params[:id])
-  end
 
   def restaurant_params
     params.require(:restaurant).permit(:name, :address, :phone_number, :category)
